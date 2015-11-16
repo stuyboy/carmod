2ce6437b577b949e7886585ccde7135c9cfeadf0
Joseph Chang <joechang@joelo.co>
2015-11-16 15:57:46 -0800

    startup a web server

--- server/http/web.go
+++ server/http/web.go
@@ -3,9 +3,33 @@ package main
 import (
 	"log"
 	"net/http"
+        "os"
+        "fmt"
 )
 
 func main() {
 	// Simple static webserver:
-	log.Fatal(http.ListenAndServe(":8080", http.FileServer(http.Dir("./client/web"))))
+        ip := os.Getenv("OPENSHIFT_GO_IP")
+        port := os.Getenv("OPENSHIFT_GO_PORT")
+        relativeDir := "./client/web"
+        cwd,cwdErr := os.Getwd()
+
+        if ip == "" {
+            ip = "localhost"
+        }
+
+        if port == "" {
+            port = "8080"
+        }
+
+        if cwdErr != nil {
+            fmt.Println(cwdErr)
+            os.Exit(1)
+        }
+
+        listenAddress := ip + ":" + port
+
+        fmt.Println("listening to " + listenAddress + " with cwd " + cwd)
+
+	log.Fatal(http.ListenAndServe(listenAddress, http.FileServer(http.Dir(relativeDir))))
 }
