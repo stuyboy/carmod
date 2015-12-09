eb18c51f7e10aff96228649796c2d0754ad46603
Joseph Chang <joechang@joelo.co>
2015-12-08 18:54:14 -0800

    working on extracting products from urls

--- server/src/carmod/api/api.go
+++ server/src/carmod/api/api.go
@@ -13,6 +13,7 @@ import (
 	"carmod/api/data"
 
 	"database/sql"
+	"log"
 )
 
 //Pointer to database
@@ -52,9 +53,30 @@ func partSearch(c web.C, w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, string(jtxt))
 }
 
+func extractPart(c web.C, w http.ResponseWriter, r *http.Request) {
+	//url := c.URLParams["url"]
+
+	url := r.URL.Query().Get("url")
+	log.Println("Trying to parse " + url)
+
+	product := data.ExtractProduct(url)
+	//product := data.ExtractProduct("https://deutscheautoparts.com/make-model-year/audi/a4/b7-2005-2008/8e0-121-403.html")
+	//product := data.ExtractProduct(`http://www.uspmotorsports.com/Engine/Software/Eurodyne-MK7-GTI-Reflash-with-Flash-Tool.html?gdftrk=gdfV210471_a_7c4200_a_7c11544_a_7cSKU4641&gclid=Cj0KEQiAnJqzBRCW0rGWnKnckOIBEiQA6qDBarUDXx6D-wqivYuGs9TsBwSbIqtyM_ZO5oMvpSUb2EcaAtPU8P8HAQ`)
+	//product := data.ExtractProduct(`http://blog.diffbot.com/diffbots-new-product-api-teaches-robots-to-shop-online/`)
+
+	log.Println("I think I am here")
+
+	//fmt.Fprintf(w, "hello world.")
+	jtxt, _ := json.Marshal(product)
+	fmt.Fprintf(w, string(jtxt))
+	//fmt.Println(product.String())
+	//log.Println(product)
+}
+
 func main() {
 	goji.Get("/hello/:name", hello)
 	goji.Get("/search/:phrase", partSearch)
 	goji.Get("/auto/:phrase", autoSearch)
+	goji.Get("/extract", extractPart)
 	goji.Serve()
 }
--- server/src/carmod/api/data/import.go
+++ server/src/carmod/api/data/import.go
@@ -4,3 +4,27 @@ package data
  Quick class that uses the diffbot api to take a URL and import parts.
  */
 
+import (
+	_ "net/url"
+	"log"
+	"github.com/diffbot/diffbot-go-client"
+)
+
+const DIFFBOT_TOKEN = "6a83c3f1b7f51319ad85a2ac80670270"
+
+func ExtractProduct(urlE string) *diffbot.Product {
+	opt := &diffbot.Options{
+		Fields: "*",
+		Timeout: 90000}
+
+	product, err := diffbot.ParseProduct(DIFFBOT_TOKEN, urlE, opt)
+
+	if err != nil {
+		if apiErr, ok := err.(*diffbot.Error); ok {
+			log.Fatal(apiErr)
+		}
+		log.Fatal(err)
+	}
+
+	return product
+}
